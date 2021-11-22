770f93f90d4e5bfd918ec3161c64fe9088747acc
Ayush <ayush2089@gmail.com>
2021-11-22 22:53:29 +0530

    New Api added

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("<h1>HELLO WORLD <samll>A small text</small?</h1>");
 });
 
+app.get("/api/v1/:id/:value", (req, res) => {
+  res.send(`Inisde a path ${req.params.id} ${req.params.value}`);
+});
+
 app.get("/api/v1/:id", (req, res) => {
   const path = req.params.id;
   res.send(`Inisde a path ${path}`);
